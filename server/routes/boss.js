a11d1eff1afb3ee0b10ee9f9d7bf28f4017e4897
Jamila Cocchiola <jamila.khawaja85@gmail.com>
2023-03-21 21:41:55 -0400

    update boss id path

--- server/routes/boss.js
+++ server/routes/boss.js
@@ -5,7 +5,7 @@ const bossRouter = express.Router()
 
 bossRouter.get('/:id', (req, res) => {
     // console.log('in boss router')
-    res.status(200).sendFile(path.resolve('public', 'boss.html'))
+    res.status(200).sendFile(path.resolve('../', 'public', 'boss.html'))
 })
 
 export default bossRouter
